7ddb9c509379e98b696cb8fe1282121e4ec94fba
marcmy <21000174+marcmy@users.noreply.github.com>
2026-07-06 19:51:36 -0400

    fix(browser): validate response URL hostnames

--- browser/background.js
+++ browser/background.js
@@ -12,11 +12,20 @@ chrome.webRequest.onHeadersReceived.addListener(
     ({ responseHeaders, type, url }) => {
         if (!responseHeaders) return;
 
-        if (type === "main_frame" && url.includes("discord.com")) {
+        let hostname;
+        try {
+            hostname = new URL(url).hostname;
+        } catch {
+            return;
+        }
+
+        const isDiscord = hostname === "discord.com" || hostname.endsWith(".discord.com");
+
+        if (type === "main_frame" && isDiscord) {
             // In main frame requests, the CSP needs to be removed to enable fetching of custom css
             // as desired by the user
             removeFirst(responseHeaders, h => h.name.toLowerCase() === "content-security-policy");
-        } else if (type === "stylesheet" && url.startsWith("https://raw.githubusercontent.com/")) {
+        } else if (type === "stylesheet" && hostname === "raw.githubusercontent.com") {
             // Most users will load css from GitHub, but GitHub doesn't set the correct content type,
             // so we fix it here
             removeFirst(responseHeaders, h => h.name.toLowerCase() === "content-type");
