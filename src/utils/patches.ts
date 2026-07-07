f9b0f35e7970098e48c9ef26bc8e99ff93b5d8d0
marcmy <21000174+marcmy@users.noreply.github.com>
2026-07-06 21:50:59 -0400

    style: restore canonical source header

--- src/utils/patches.ts
+++ src/utils/patches.ts
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+*/
 
 import { runtimeHashMessageKey } from "./intlHash";
 import { Patch, PatchReplacement, ReplaceFn } from "./types";
